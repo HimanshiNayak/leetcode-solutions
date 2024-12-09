501185695f764afab0fc2332ec9dbde4597af014
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2024-12-09 23:34:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (16.84%) - LeetSync

--- 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.cpp
@@ -1,7 +1,34 @@
 class Solution {
+  private:    
+    bool isPalindromicInBase(int n, int base){
+      //usko jis base mei convert krna hai usko rem method is krke vec mei store krlo
+      vector<int>digits;
+      while(n>0){
+        digits.push_back(n%base);
+        n/=base;
+        //now check palindrome
+        int l =0;
+        int r = digits.size()-1;
+        while(l<r){
+          if(digits[l]!=digits[r])return false;
+          l++;
+          r--;
+        }
+    
+      }
+       return true;
+    }
 public:
     bool isStrictlyPalindromic(int n) {
-        return false ; //as never possible mathematecally
+//         return false ; //as never possible mathematecally
+      
+      //ab 2 se leke n-2 ke liye chla do loop aue check krlo
+      for(int base=2;base<=n-2;base++){
+        if(!isPalindromicInBase(n,base)){
+          return false;
+        }
+      }
+      return true;
       
     }
 };
