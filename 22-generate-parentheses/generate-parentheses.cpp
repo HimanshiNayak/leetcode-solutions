23017f435c3bc9eab5d310e83bb9129e8cf13d45
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-01-22 10:14:01 +0530

    Time: 5 ms (18.91%) | Memory: 16.6 MB (12.64%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+// vector<string>res;
+// void valid(int open , int close , string ans , vector<string>res){
+//     if(open ==0 && open ==0){
+//         res.push_back(ans);
+//         return ;
+//     }
+//     if(open>0){
+//         ans.push_back('(');
+//         valid(open-1,close, ans , res);
+//         ans.pop_back();
+//     }
+//     if(close>open){
+//           ans.push_back(')');
+//         valid(open,close-1, ans , res);
+//         ans.pop_back();
+//     }
+// }
+//     vector<string> generateParenthesis(int n) {
+//         int open =n;
+//         int close = n;
+//         string ans ;
+//         valid(open , close , ans , res);
+//         return res;
+//     }
+// };
+class Solution {
+public:
+    vector<string> res;
+
+    void valid(int open, int close, string ans, vector<string>& res) {
+        if (open == 0 && close == 0) {
+            res.push_back(ans);
+            return;
+        }
+        if (open > 0) {
+            ans.push_back('(');
+            valid(open - 1, close, ans, res);
+            ans.pop_back(); // Backtrack
+        }
+        if (close > open) {
+            ans.push_back(')');
+            valid(open, close - 1, ans, res);
+            ans.pop_back(); // Backtrack
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        int open = n, close = n;
+        string ans;
+        valid(open, close, ans, res);
+        return res;
+    }
+};
