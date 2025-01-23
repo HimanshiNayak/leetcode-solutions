7ae582e273eeb2eb8bbed30300b1ab19b5d10653
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-01-23 20:08:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (42.91%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        
-        if(n < 0) {
-            x = 1 / x;
-        } 
-        
-        long num = labs(n);
-        
-        double pow = 1;
-        
-        while(num){ // equivalent to while(num != 0)
-            if(num & 1) { // equivalent to if((num & 1) != 0)
-                pow *= x;
-            }
-            
-            x *= x;
-            num >>= 1;
+double myPow(double x, int n) {
+    double ans = 1.0;
+    long nn = n;
+    if(nn<0)nn = -1*nn;
+    while(nn>0){
+        if(nn%2 == 1){
+            ans = ans*x;
+            nn = nn-1;
         }
-        
-        return pow;
+        else{
+            x = x*x;
+            nn= nn/2;
+        }
     }
+    if(n<0)ans =(double)(1.0)/(double)(ans);
+    return ans;
+      
+}
 };
