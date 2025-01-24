4cddef05cbd562420d69b6ffff6fa7bdaba0d505
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-01-24 10:12:20 +0530

    Time: 86 ms (7.10%) | Memory: 286.7 MB (5.01%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int helper(string s, int& idx) {
+        int result = 0;
+        int sign = 1;
+        int number = 0;
+
+        for (; idx < s.length(); idx++) {
+            char currentChar = s[idx];
+            if (currentChar == ' ') continue;
+            else if (currentChar == '+' || currentChar == '-') {
+                result += sign * number;
+                number = 0;
+                sign = (currentChar == '-') ? -1 : 1;
+            } else if (currentChar == '(') {
+                result += sign * helper(s, ++idx);
+            } else if (currentChar == ')') {
+                break;
+            } else {
+                number = number * 10 + (currentChar - '0');
+            }
+        }
+        return result + sign * number;
+    }
+
+    int calculate(string s) {
+        int idx = 0;
+        return helper(s, idx);
+    }
+};
