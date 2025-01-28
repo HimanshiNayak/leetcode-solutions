214e352300906323634633ebc39b303d4c21594c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-01-28 10:21:48 +0530

    Time: 23 ms (79.10%) | Memory: 27.6 MB (28.45%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -22,18 +22,38 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); 
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1)); 
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1; 
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); 
-                }
-            }
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= m; j++) {
+//                 if (text1[i - 1] == text2[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1] + 1; 
+//                 } else {
+//                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]); 
+//                 }
+//             }
+//         }
+
+//         return dp[n][m]; 
+//     }
+// };
+
+for(int i=0;i<n;i++){
+    dp[i][m]=0;
+}
+for(int j=0;j<m;j++){
+    dp[n][j]=0;
+}
+for(int  i= n-1;i>=0;i--){
+    for(int j = m-1;j>=0;j--){
+        if(text1[i]==text2[j]){
+            dp[i][j] = 1+ dp[i+1][j+1];
         }
-
-        return dp[n][m]; 
+        else{
+            dp[i][j] = max(dp[i][j+1] , dp[i+1][j]);
+        }
+    }
+}
+return dp[0][0];
     }
 };
