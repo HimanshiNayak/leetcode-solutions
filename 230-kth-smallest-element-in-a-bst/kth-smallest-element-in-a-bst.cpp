7e94baa7941e140ff7c62490b4933e2a692362af
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-02-15 11:22:59 +0530

    Time: 0 ms (100.00%) | Memory: 24.3 MB (90.22%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -13,16 +13,35 @@
 
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int>& v) {
-        if (root == nullptr) return;
-        inorder(root->left, v);
-        v.push_back(root->val);
-        inorder(root->right, v);
-    }
+//space o(n)
+    // void inorder(TreeNode* root, vector<int>& v) {
+    //     if (root == nullptr) return;
+    //     inorder(root->left, v);
+    //     v.push_back(root->val);
+    //     inorder(root->right, v);
+    // }
 
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> v;
-        inorder(root, v);
-        return v[k - 1]; 
+    // int kthSmallest(TreeNode* root, int k) {
+    //     vector<int> v;
+    //     inorder(root, v);
+    //     return v[k - 1]; 
+    // }
+
+    //const space o(1)
+    void solve (TreeNode* root , int& ans ,int &cnt,int k ){
+        if(root==NULL)return;
+        solve(root->left,ans , cnt,k);
+        cnt++;
+        if(cnt==k){
+            ans = root->val;
+            return;
+        }
+        solve(root->right,ans,cnt,k);
     }
+     int kthSmallest(TreeNode* root, int k) {
+        int cnt =0;
+        int ans;
+        solve(root,ans,cnt,k);
+        return ans;
+     }
 };
