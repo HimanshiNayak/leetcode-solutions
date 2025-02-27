fab445d0108c2f491cf4920b51d53ff55f914b88
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-02-27 15:33:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (25.12%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,56 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = new ListNode(0);
-        temp->next= head;
-        ListNode* fast = temp;
-        ListNode* slow = temp;
-        int curr=0;
-        for(int i=0;i<=n;i++){
+        ListNode* dummy = new ListNode(0); // Dummy node to handle edge cases
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        // Move fast pointer 'n' steps ahead
+        for (int i = 0; i < n; i++) {
             fast = fast->next;
         }
-        while(fast!= NULL){
+
+        // Move both fast and slow until fast reaches the end
+        while (fast->next != NULL) {
             fast = fast->next;
-            slow=slow->next;
+            slow = slow->next;
         }
+
+        // Remove the nth node
         slow->next = slow->next->next;
-        return temp->next;
+
+        return dummy->next; // Return the new head
     }
 };
-
-
-// class Solution {
-// public:
-//     ListNode* removeNthFromEnd(ListNode* head, int n) {
-//         ListNode* dummy = new ListNode(0); // Dummy node to handle edge cases
-//         dummy->next = head;
-//         ListNode* fast = dummy;
-//         ListNode* slow = dummy;
-
-//         // Move fast pointer 'n' steps ahead
-//         for (int i = 0; i <= n; i++) {
-//             fast = fast->next;
-//         }
-
-//         // Move both fast and slow until fast reaches the end
-//         while (fast != NULL) {
-//             fast = fast->next;
-//             slow = slow->next;
-//         }
-
-//         // Remove the nth node
-//         slow->next = slow->next->next;
-
-//         return dummy->next; // Return the new head
-//     }
-// };
