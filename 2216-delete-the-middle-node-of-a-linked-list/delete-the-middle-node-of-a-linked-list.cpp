785e8e336af193aeb91c1a45eb93418f099a6788
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-03-02 19:46:33 +0530

    Time: 3 ms (45.14%) | Memory: 311.9 MB (83.39%) - LeetSync

---
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+//tortoise and hare but with fast as fast->next->next from start , slow head pe hi , so thaut we stopone beforemid
+    ListNode* deleteMiddle(ListNode* head) {
+        if(!head || !head->next)return nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        fast = fast->next->next;
+        while(fast!=NULL && fast->next !=NULL){
+            slow = slow->next;
+            fast= fast->next->next;
+        }
+        slow->next= slow->next->next;
+        return head;
+    }
+};
