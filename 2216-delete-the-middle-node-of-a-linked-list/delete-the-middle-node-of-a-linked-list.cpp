5a698d972e2d98b3fc2fff392a8e6115fa47e6c4
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-03-02 19:48:10 +0530

    Time: 8 ms (8.21%) | Memory: 311.9 MB (98.46%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -12,15 +12,34 @@ class Solution {
 public:
 //tortoise and hare but with fast as fast->next->next from start , slow head pe hi , so thaut we stopone beforemid
     ListNode* deleteMiddle(ListNode* head) {
-        if(!head || !head->next)return nullptr;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        fast = fast->next->next;
-        while(fast!=NULL && fast->next !=NULL){
-            slow = slow->next;
-            fast= fast->next->next;
-        }
-        slow->next= slow->next->next;
-        return head;
-    }
+//         if(!head || !head->next)return nullptr;
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+//         fast = fast->next->next;
+//         while(fast!=NULL && fast->next !=NULL){
+//             slow = slow->next;
+//             fast= fast->next->next;
+//         }
+//         slow->next= slow->next->next;
+//         return head;
+//     }
+// };
+
+if (!head || !head->next) return NULL;
+  ListNode* temp = head;
+  int count=0,n=0;
+  while(temp){
+      count++;
+      temp= temp->next;
+  }
+ 
+  n = count/2;
+  temp = head;
+  for(int i=0;i<n-1;i++){temp = temp->next;}
+  
+  ListNode* mid = temp->next;
+  temp->next= temp->next->next;
+  delete mid;
+  return head;
+}
 };
