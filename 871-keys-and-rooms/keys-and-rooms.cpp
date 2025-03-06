649e0562e651234b362067951768bb02183ec76f
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-03-06 14:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (59.07%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) { //bfs
+        int n = rooms.size();
+        vector<int>vis(n,0);
+        queue<int>q;
+        q.push(0);
+        vis[0] =1;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it: rooms[node]){
+                if(!vis[it]){
+                   q.push(it);
+                   vis[it]=1;
+                }
+            }
+        }
+        int number = count(vis.begin(),vis.end(),1);
+    return number==n;
+    }
+};
