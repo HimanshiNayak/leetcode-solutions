f2b77722c534ceee94558f7ec5f5998a3de08b5b
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-11-11 12:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (58.23%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -5,9 +5,8 @@ public:
         vector<int>ans(2*n);
         int i =0;
         while(i<n){
-int j = i+n;
-ans[i]= nums[i];
-ans[j]= nums[i];
+ans[i]=ans[i+n]=nums[i];
+
 i++;
         }
         return ans;
