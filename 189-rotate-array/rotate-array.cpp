7afb665be582bf78a48b5d0a3c2c012328740213
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-11-26 01:44:10 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (36.17%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int>temp;
-        k = k%n;
-        for(int i = n-k ;i <n;i++){
-            temp.push_back(nums[i]);
-        } 
-        for(int i=0;i<n-k;i++){
-            temp.push_back(nums[i]);
-        }
-        nums = temp;
+    // void rotate(vector<int>& nums, int k) {
+        // // ek temp vec usmei last utte ele rkh do 
+        // int n = nums.size();
+        // vector<int>temp;
+        // k = k%n;
+        // for(int i = n-k ;i <n;i++){
+        //     temp.push_back(nums[i]);
+        // } 
+        // for(int i=0;i<n-k;i++){
+        //     temp.push_back(nums[i]);
+        // }
+        // nums = temp;
+
+        // rev poori array , then rev k ele then n-k ele
+        void rotate(vector<int>& nums, int k) {
+        k = k%nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k); //not k-1 as iter wount inclde end wala
+        reverse(nums.begin()+k, nums.end());
     }
 };
