1deb05c13a37edaa8da798cdc41ae2af0e510992
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-11-28 22:44:17 +0530

    Time: 2 ms (3.57%) | Memory: 11.7 MB (11.99%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+// dutch national flag algo : take three var 0 , 1 ,2 ; f mid is 0 swap with low and low ++, if mid 1 then ++ if 2 then swap with high and high -- , 
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+    }
+};
