98753eb9572f5adb8af163747f3b1f89b97d547e
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-11-30 23:47:11 +0530

    Time: 0 ms (100.00%) | Memory: 97.2 MB (86.79%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,26 +4,31 @@ public:
         // Function to calculate maximum profit using single pass
 
         // Initialize the minimum price to a large number
-        int minPrice = INT_MAX;
+        int maxi = INT_MIN;
 
         // Initialize the maximum profit to 0
-        int maxProfit = 0;
+        int mini = INT_MAX;
 
-        // Traverse each price in the array
-        for (int price : prices) {
-            // If current price is less than minPrice, update minPrice
-            if (price < minPrice) {
-                minPrice = price;
-            }
-            // Else calculate profit and update maxProfit if it's greater
-            else {
-                maxProfit = max(maxProfit, price - minPrice);
-            }
-        }
+        // // Traverse each price in the array
+        // for (int price : prices) {
+        //     // If current price is less than minPrice, update minPrice
+        //     if (price < minPrice) {
+        //         minPrice = price;
+        //     }
+        //     // neg nhi include
+        //     else {
+        //         maxProfit = max(maxProfit, price - minPrice);
+        //     }
+        // }
 
-        // Return the maximum profit found
-        return maxProfit;
+        // // Return the maximum profit found
+        // return maxProfit;
  
-
+for(int i=0;i<prices.size();i++){
+    mini = min(mini , prices[i]);
+    int diff= prices[i]-mini;
+    maxi = max(maxi , diff);
+}
+return maxi;
     }
 };
