fe00b89f0f17e8ca214074900df7938b30253731
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-06 02:21:15 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (70.49%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,17 +1,24 @@
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+// int i = m-1;
+// int j = n-1;
+// int k= m+n-1;
+// while(i>=0 && j>=0){
+//     if (nums2[j]>nums1[i])nums1[k--]=nums2[j--];
+//     else {nums1[k--]=nums1[i--];}
+// }
+//     while(j>=0){
+//         nums1[k--]=nums2[j--];
+
+// }
+    
+//     }
+// };
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-int i = m-1;
-int j = n-1;
-int k= m+n-1;
-while(i>=0 && j>=0){
-    if (nums2[j]>nums1[i])nums1[k--]=nums2[j--];
-    else {nums1[k--]=nums1[i--];}
-}
-    while(j>=0){
-        nums1[k--]=nums2[j--];
-
-}
-    
+        copy(nums2.begin(), nums2.end(), nums1.begin() + m);
+        sort(nums1.begin(), nums1.end());
     }
 };
