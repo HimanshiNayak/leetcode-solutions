ae55405c299c2492cc0b7b4b678e3584e8837f36
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-07 23:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (3.40%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int cnt =0;
-        for(int i=low;i<=high;i++){
-            if(i%2!=0)cnt++;
-        }
-        return cnt;
-    }
+        // int cnt =0;
+        // for(int i=low;i<=high;i++){
+        //     if(i%2!=0)cnt++;
+        // }
+        // return cnt;
+
+        //will have half ec=vene and hald odd , ut when both odd then one exra odd
+        int nums = high-low+1;
+        if(low%2!=0 && high %2!=0)return nums/2 +1;
+        else return nums/2 ; }
 };
