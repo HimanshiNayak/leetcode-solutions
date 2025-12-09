3d68aac5aaecc08601f489bf1f7b58df08d7694e
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-10 00:59:51 +0530

    Time: 3 ms (2.90%) | Memory: 19.3 MB (20.32%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)return true;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==target)return true;
+        // }
+        // return false;
+unordered_map<int , int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+        for(auto x:mp){
+            if(mp.find(target) != mp.end())return true;
         }
         return false;
     }
