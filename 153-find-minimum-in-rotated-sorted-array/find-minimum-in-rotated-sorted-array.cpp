db04127fe7c150f3711eb92c5dc089aebcf7971d
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-10 01:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (78.45%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-      
-    // int low = 0, high = nums.size() - 1;
-    // int ans = INT_MAX;
-    // while (low <= high) {
-    //     int mid = (low + high) / 2;
-    //     //search space is already sorted
-    //     //then arr[low] will always be
-    //     //the minimum in that search space:
-    //     if (nums[low] <= nums[high]) {
-    //         ans = min(ans, nums[low]);
-    //         break;
-    //     }
-
-    //     //if left part is sorted:
-    //     if (nums[low] <= nums[mid]) {
-    //         // keep the minimum:
-    //         ans = min(ans, nums[low]);
-    //         // Eliminate left half:
-    //         low = mid + 1;
-    //     }
-    //     else { //if right part is sorted:
-
-    //         // keep the minimum:
-    //         ans = min(ans, nums[mid]);
-
-    //         // Eliminate right half:
-    //         high = mid - 1;
-    //     }
-    // }
-    // return ans;
-sort(nums.begin(),nums.end());
-
-return nums[0];
-
+        // if mid >end then right jao else left jao , this kyuki rotated mei phle bde and last mei chote
+        int s =0;
+        int e = nums.size()-1;
+        while(s<e){
+            int mid = s+ (e-s)/2;
+            if(nums[mid]>nums[e])s = mid+1;
+            else e = mid;
+        }
+        return nums[s];
     }
 };
