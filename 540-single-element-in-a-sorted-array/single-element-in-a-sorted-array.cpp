16252d3e36555843974729ed63e0f5398cd1013c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-10 23:33:57 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.72%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,13 +1,24 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums){
+//     unordered_map <int,int>mp;
+//     for(int x:nums){
+//         mp[x]++;
+//     }
+//     for(auto x:mp){
+//         if(x.second==1)return x.first;
+//     }
+//     return -1;
+//     }
+// };
+
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums){
-    unordered_map <int,int>mp;
-    for(int x:nums){
-        mp[x]++;
-    }
-    for(auto x:mp){
-        if(x.second==1)return x.first;
-    }
-    return -1;
+    int singleNonDuplicate(vector<int>& nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;     
     }
 };
