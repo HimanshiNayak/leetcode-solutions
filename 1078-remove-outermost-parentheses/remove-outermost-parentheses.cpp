e44c558886ecd0c9376fc09f728e27691551b89f
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-19 23:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (52.80%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-     string result = "";
-    int depth = 0; // Variable to track the depth of parentheses
-    
-    for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '(') {
-            if(depth > 0) {
-                // Append '(' only if it's not an outer parenthesis
-                result += '(';
-            }
-            depth++; // Increase depth for every opening parenthesis
-        } else if(s[i] == ')') {
-            depth--; // Decrease depth for every closing parenthesis
-            if(depth > 0) {
-                // Append ')' only if it's not an outer parenthesis
-                result += ')';
+        string res = "";
+        int cnt = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                if (cnt > 0) res += c;
+                cnt++;
+            } else {
+                cnt--;
+                if (cnt > 0) res += c;
             }
         }
-    }
-    
-    return result;
+        return res;
     }
 };
