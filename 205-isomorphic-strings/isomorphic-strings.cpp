ce67c7c7c13c05bfff94dad849b45f1bffc400d2
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-21 21:39:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (54.44%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size())return false;
-        vector<int>s1(256,0);
-          vector<int>s2(256,0);
-        for(int i=0;i<s.size();i++){
-            if(s1[s[i]] != s2[t[i]]){
-                return false;
+        // using freq array
+        // if(s.size()!=t.size())return false;
+        // vector<int>s1(256,0);
+        //   vector<int>s2(256,0);
+        // for(int i=0;i<s.size();i++){
+        //     if(s1[s[i]] != s2[t[i]]){
+        //         return false;
+        //     }
+        //     s1[s[i]]=i+1;
+        //     s2[t[i]] = i+1; 
+        // }
+        // return true;
+
+        //using hasmap
+
+        unordered_map<char , char>charMap;
+        for(int i=0;i<s.length();i++){
+            //see if this s strings char in hasmap already 
+            if(charMap.count(s[i])){
+                //check if it has the same char in value
+                if(charMap[s[i]]!= t[i])return false;
             }
-            s1[s[i]]=i+1;
-            s2[t[i]] = i+1; 
+            //see if the value is ale=ready mapped with someelse
+            else{
+                for(auto& pair:charMap ){
+                    if(pair.second == t[i])return false;
+                }
+            }
+            charMap[s[i]] = t[i];
         }
         return true;
     }
