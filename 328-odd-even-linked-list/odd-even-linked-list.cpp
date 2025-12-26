0ac63fa7f7daff25e6017a4ee4685bcdecc86b26
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-26 22:10:12 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (27.02%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,44 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-    //using vector:as addtional space first to store all nodes then odd and even ones and then 
-    //    ListNode* temp = head;
-    //    vector<int>v,even,odd;
-    //    while(temp){
-    //     v.push_back(temp->val);
-    //     temp = temp->next;
-    //    }
-    //    int n = v.size();
-    //   for (int i = 0; i < n; i += 2) odd.push_back(v[i]);
-    //   for (int i = 1; i < n; i += 2) even.push_back(v[i]);
-    //   temp = head;
-    //   int i=0,j=0;
-    //   while(i<odd.size()){
-    //       temp->val = odd[i];
-    //       temp = temp->next;
-    //       i++;
-    //   }
-    //   while(j<even.size()){
-    //     temp->val = even[j];
-    //     temp = temp->next;
-    //     j++;
-    //   }
-    //   return head;
-    // }
+ 
+        if (!head) return head;
 
-    //two pinters ..one to odd head and other to even head
+        vector<int> vals;
+        ListNode* temp = head;
 
-    if(!head || !head->next)return head;
-    ListNode* oddhead = head;
-    ListNode* even = head->next;
-    ListNode* evenhead = even;
-    while(even && even->next){
-        oddhead->next = even->next;
-        oddhead = oddhead->next;
-        even->next = oddhead->next;
-        even = even->next;
-    }
-oddhead->next = evenhead;
-return head;
+        // Store all values
+        while (temp) {
+            vals.push_back(temp->val);
+            temp = temp->next;
+        }
+
+        temp = head;
+
+        // Place odd-indexed values (0, 2, 4...)
+        for (int i = 0; i < vals.size(); i += 2) {
+            temp->val = vals[i];
+            temp = temp->next;
+        }
+
+        // Place even-indexed values (1, 3, 5...)
+        for (int i = 1; i < vals.size(); i += 2) {
+            temp->val = vals[i];
+            temp = temp->next;
+        }
+
+        return head;
     }
 };
