798aadb299d99b60776479c1ad6bfc5ae2c75016
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-30 22:02:27 +0530

    Time: 55 ms (32.72%) | Memory: 23.9 MB (34.49%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -8,6 +8,25 @@
  */
 class Solution {
 public:
+
+
+//length diff ka func
+int lengdiff(ListNode *headA, ListNode *headB){
+    int len1=0, len2=0;
+    ListNode* temp= headA;
+     while (headA != NULL || headB != NULL) {
+        if (headA != NULL) {
+            ++len1;
+            headA = headA->next;
+        }
+        if (headB != NULL) {
+            ++len2;
+            headB = headB->next;
+        }
+    }
+    return len1-len2;
+}
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         //ek ko set mei bhr do fir dekh lo agr durse ka koi node prt hai ya ni  //insert listnode not val only
 
@@ -25,14 +44,32 @@ public:
         // return NULL;
 
 
-        //otimal dono ko traverse kro ..jo khmtm ho jaye usko dusrehead pe dal do , chlo until they meet
+        // //otimal dono ko traverse kro ..jo khmtm ho jaye usko dusrehead pe dal do , chlo until they meet
 
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-         while(temp1!= temp2){
-            temp1= temp1==NULL?headB:temp1->next;
-            temp2 = temp2==NULL?headA:temp2->next;
-         }
-         return temp1;
+        // ListNode* temp1 = headA;
+        // ListNode* temp2 = headB;
+        //  while(temp1!= temp2){
+        //     temp1= temp1==NULL?headB:temp1->next;
+        //     temp2 = temp2==NULL?headA:temp2->next;
+        //  }
+        //  return temp1;
+
+//space km krne ke liye diff nikal lo lenghth ka fir utne ke baad se chlao agr mil gye toh return nhi toh null
+
+//if neg second wale ko move if pos firts ko move
+
+int diff = lengdiff(headA, headB);
+
+if(diff>0)
+    while(diff-- != 0)headA= headA->next;
+else 
+    while(diff++ !=0)headB = headB->next;
+
+while(headB!=NULL){
+if(headA==headB)return headA;
+headA=headA->next;
+headB= headB->next;
+}
+return NULL;
     }
 };
