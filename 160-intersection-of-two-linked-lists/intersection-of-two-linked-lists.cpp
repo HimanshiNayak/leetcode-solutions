a6055089e255159fdf5334adae7743cbe94517b6
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2025-12-30 21:42:00 +0530

    Time: 51 ms (41.76%) | Memory: 23.9 MB (22.79%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -11,17 +11,28 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         //ek ko set mei bhr do fir dekh lo agr durse ka koi node prt hai ya ni  //insert listnode not val only
 
-        unordered_set<ListNode*>s;
-        ListNode* temp = headA;
-        while(temp){
-            s.insert(temp);
-            temp= temp->next;
-        }
-        temp = headB;
-        while(temp){
-            if(s.find(temp)!=s.end())return temp;
-            temp = temp->next;
-        }
-        return NULL;
+        // unordered_set<ListNode*>s;
+        // ListNode* temp = headA;
+        // while(temp){
+        //     s.insert(temp);
+        //     temp= temp->next;
+        // }
+        // temp = headB;
+        // while(temp){
+        //     if(s.find(temp)!=s.end())return temp;
+        //     temp = temp->next;
+        // }
+        // return NULL;
+
+
+        //otimal dono ko traverse kro ..jo khmtm ho jaye usko dusrehead pe dal do , chlo until they meet
+
+        ListNode* temp1 = headA;
+        ListNode* temp2 = headB;
+         while(temp1!= temp2){
+            temp1= temp1==NULL?headB:temp1->next;
+            temp2 = temp2==NULL?headA:temp2->next;
+         }
+         return temp1;
     }
 };
