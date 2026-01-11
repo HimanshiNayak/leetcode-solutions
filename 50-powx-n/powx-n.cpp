171bc7ac893b9dd1de2269b1211a25b641ceac2c
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2026-01-12 00:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.26%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-double myPow(double x, int n) {
-    double ans = 1.0;
-    long nn = n;
-    if(nn<0)nn = -1*nn;
-    while(nn>0){
-        if(nn%2 == 1){
-            ans = ans*x;
-            nn = nn-1;
+    double myPow(double x, int n) {
+        //even power gets n/2 and the x is multoiplied by itself
+        //odd n = n-1 and ans is multiplied by x
+
+        double ans = 1.0;
+        long nn = n;
+        if(nn<0)nn = -1*nn;
+        while(nn>0){
+            if(nn%2==1){
+                ans = ans * x;
+                nn = nn-1;
+            }
+            else{
+                x= x*x;
+                nn= nn/2;
+            }
         }
-        else{
-            x = x*x;
-            nn= nn/2;
-        }
+            if (n<0)ans= double(1.0)/double(ans);
+ return ans;
     }
-    if(n<0)ans =(double)(1.0)/(double)(ans);
-    return ans;
-      
-}
 };
