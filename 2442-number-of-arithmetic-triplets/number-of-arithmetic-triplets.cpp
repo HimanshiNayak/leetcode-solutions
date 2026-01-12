dc671bee02ac119707747e833fa28afb6e6b6dda
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2026-01-13 03:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (27.42%) - LeetSync

---
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -0,0 +1,17 @@
+//2 diff as first and last ka diff whi hai
+        class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff) {
+        unordered_map <int, int> seen;
+        int counter = 0;
+        for (int i = 0; i < nums.size(); i++){
+            seen[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++){
+            if(seen.count(nums[i] + diff) && seen.count(nums[i] + (2*diff))){
+                counter++;
+            }
+        }
+        return counter;
+    }
+};
