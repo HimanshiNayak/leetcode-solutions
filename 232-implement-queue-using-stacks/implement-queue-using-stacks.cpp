2e8021d6ffb75abf7254cecbc22f01cff2619bf7
Himanshi Nayak <114794957+HimanshiNayak@users.noreply.github.com>
2026-02-06 02:13:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (67.97%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -3,32 +3,35 @@ public:
     MyQueue() {
         
     }
-  stack<int>s1,s2;
-    
+    stack<int>s1 , s2;
     void push(int x) {
-        while(!s1.empty()){   // put all values of s1 in s2 for time being
-            s2.push(s1.top());
-            s1.pop();
-        }
-        s1.push(x);       // push element to the bottom of s1
-        while(!s2.empty()){  // get back all value form s2
-            s1.push(s2.top());
-            s2.pop();
-        }
+        s1.push(x);
     }
     
     int pop() {
-     int n = s1.top();
-      s1.pop();
-    return n;
+        if(s2.empty()){
+            while(s1.size()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int val = s2.top();
+        s2.pop();
+        return val;
     }
     
     int peek() {
-        return s1.top();
+            if(s2.empty()){
+            while(s1.size()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+         return s2.top();
     }
     
     bool empty() {
-        return s1.empty();
+       return s1.empty() && s2.empty();
     }
 };
 
